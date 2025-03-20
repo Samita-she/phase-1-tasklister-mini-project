4649dd65a1cab8fa482e1606de42c716f36c2001
Sheila Samita <samitasheila1@gmail.com>
2025-03-20 07:40:38 +0300

    Updated package.json

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
 });
+  const form = document.getElementById("create-task-form");
+  const taskList = document.getElementById("tasks");
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault(); 
+
+    const newTask = document.getElementById("new-task-description").value;
+
+    const taskItem = document.createElement("li");
+    taskItem.textContent = newTask;
+
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "❌";
+    deleteButton.addEventListener("click", () => taskItem.remove());
+
+    taskItem.appendChild(deleteButton);
+
+    taskList.appendChild(taskItem);
+
+    form.reset();
+  });
+
+
